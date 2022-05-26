a0a4984e6789622c6a960c924f28766035448a1a
Andrew Ivasiv <andriy.ivasiv@lemberg.co.uk>
2022-05-26 17:27:03 +0300

    Optimize partners randomizer js for case with less than 7 items

--- docroot/themes/custom/themekit/partials/views/partners/view--partners.js
+++ docroot/themes/custom/themekit/partials/views/partners/view--partners.js
@@ -7,18 +7,20 @@ Drupal.behaviors.themekitPartners = {
     const wrapper = view.querySelector('.view-content');
     const items = view.querySelectorAll('.views-row');
     if (!items) return;
-    const itemsRange = items.length - 1;
-    while (randomIndex.length !== itemsToShow) {
-      const randomNumber = Math.floor(Math.random() * (itemsRange + 1));
-      if (!randomIndex.includes(randomNumber)) {
-        randomIndex.push(randomNumber);
+    const itemsRange = items.length;
+    if (itemsRange > itemsToShow) {
+      while (randomIndex.length !== itemsToShow) {
+        const randomNumber = Math.floor(Math.random() * itemsRange);
+        if (!randomIndex.includes(randomNumber)) {
+          randomIndex.push(randomNumber);
+        }
       }
+      items.forEach((item, index) => {
+        if (!randomIndex.includes(index)) {
+          item.remove();
+        }
+      });
     }
-    items.forEach((item, index) => {
-      if (!randomIndex.includes(index)) {
-        item.remove();
-      }
-    });
     wrapper.classList.add('processed');
   },
 };
