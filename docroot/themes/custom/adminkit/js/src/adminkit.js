87427c970e5d97dfe7a04dd943805ebadcdf350c
Roman Paska <roman.paska@lembergsolutions.com>
2022-09-23 14:03:48 +0300

    COD-42 Set drupal/gin as a default admin theme; remove not needed Paragon themes

--- docroot/themes/custom/adminkit/js/src/adminkit.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file
- * Entry point for all admin theme related js.
- */
-
-require('./draggable-items');
-require('./expandable-paragraphs');
-// require('./form-validation');
--- docroot/themes/custom/adminkit/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  sourceMap: true,
-  parser: 'postcss-scss',
-  plugins: [
-    require('autoprefixer'),
-    require('postcss-safe-parser'),
-  ],
-};
